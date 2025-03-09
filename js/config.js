89e89716762ba4594dd24402adb4e40487a0171f
theaterish <hello@theaterish.com>
2025-03-09 09:07:15 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -186,3 +186,24 @@ if ('serviceWorker' in navigator) {
       });
   });
 }
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCDF9_1SAsjkBWnMXvmCJqMuvZpSUyRa3o",
+  authDomain: "theatre-check-in-app.firebaseapp.com",
+  projectId: "theatre-check-in-app",
+  storageBucket: "theatre-check-in-app.firebasestorage.app",
+  messagingSenderId: "927794666232",
+  appId: "1:927794666232:web:7c19a30c3d6fed6d62f90d",
+  measurementId: "G-194L57FXCH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
